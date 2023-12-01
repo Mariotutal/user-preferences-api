4a267a9743e0adb8dc542a97ec6df72b2f8dfd10
MarioVargas <drakenmario@gmail.com>
2023-12-01 00:04:07 -0600

    Fix for deployment 4.0

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import app from "./routes";
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 //Start the server
 app.listen(port, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running at port ${port}`);
 });
